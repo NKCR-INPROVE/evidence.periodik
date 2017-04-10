24223b9394cf6bad9a9b4233d3190278a11b7cd0
Alberto Hernandez <alberto@albertoX1>
2017-04-10 10:07:26 +0200

    organizing

--- searchapp/src/main/ngClient/src/app/app.module.ts
+++ searchapp/src/main/ngClient/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BsDropdownModule } from 'ng2-bootstrap';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OCasopisuComponent } from './components/o-casopisu/o-casopisu.component';
+import { PokynyComponent } from './components/pokyny/pokyny.component';
+import { ArchivComponent } from './components/archiv/archiv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OCasopisuComponent,
+    PokynyComponent,
+    ArchivComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ searchapp/src/main/ngClient/src/app/components/o-casopisu/o-casopisu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-o-casopisu',
+  templateUrl: './o-casopisu.component.html',
+  styleUrls: ['./o-casopisu.component.scss']
+})
+export class OCasopisuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

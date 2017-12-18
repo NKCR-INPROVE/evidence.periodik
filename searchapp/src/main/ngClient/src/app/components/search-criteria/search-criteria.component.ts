6c685f7f422d3a4cb3213a6875da91033a6c0b02
Alberto Hernandez <alberto@albertoX1>
2017-12-18 14:48:24 +0100

    fix podle mail

--- searchapp/src/main/ngClient/src/app/components/search-criteria/search-criteria.component.ts
+++ searchapp/src/main/ngClient/src/app/components/search-criteria/search-criteria.component.ts
@@ -1,130 +1,130 @@
-import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
-import { Subscription } from 'rxjs/Subscription';
+import {Component, OnInit, OnDestroy, Output, EventEmitter} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Observable} from 'rxjs/Rx';
+import {Subscription} from 'rxjs/Subscription';
 
-import { Criterium } from '../../models/criterium';
-import { AppState } from '../../app.state';
-import { AppService } from '../../services/app.service';
+import {Criterium} from '../../models/criterium';
+import {AppState} from '../../app.state';
+import {AppService} from '../../services/app.service';
 
 @Component({
-  selector: 'app-search-criteria',
-  templateUrl: './search-criteria.component.html',
-  styleUrls: ['./search-criteria.component.scss']
+    selector: 'app-search-criteria',
+    templateUrl: './search-criteria.component.html',
+    styleUrls: ['./search-criteria.component.scss']
 })
 export class SearchCriteriaComponent implements OnInit, OnDestroy {
 
-  //@Input() criterium: Criterium;
-  @Output() onSearch: EventEmitter<Criterium[]> = new EventEmitter<Criterium[]>();
-  subscriptions: Subscription[] = [];
+    //@Input() criterium: Criterium;
+    @Output() onSearch: EventEmitter<Criterium[]> = new EventEmitter<Criterium[]>();
+    subscriptions: Subscription[] = [];
 
-  criteria: Criterium[] = [];
-  fields = [
-    { field: '_text_', label: 'kdekoliv' },
-    { field: 'title', label: 'název' },
-    { field: 'autor', label: 'autor' },
-    { field: 'keywords', label: 'klíčová slova' },
-    { field: 'genre', label: 'rubrika' },
-    { field: 'ocr', label: 'plný text dokumentu' }
-  ]
-  
-  operators = [
-    { val: 'AND', label: 'a zároveň' },
-    { val: 'OR', label: 'nebo' }
-  ]
+    criteria: Criterium[] = [];
+    fields = [
+        {field: '_text_', label: 'kdekoliv'},
+        {field: 'title', label: 'název'},
+        {field: 'autor', label: 'autor'},
+        {field: 'keywords', label: 'klíčová slova'},
+        {field: 'genre', label: 'rubrika'},
+        {field: 'ocr', label: 'plný text dokumentu'}
+    ]
 
-  constructor(
-    private state: AppState,
-    private service: AppService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+    operators = [
+        {val: 'AND', label: 'a zároveň'},
+        {val: 'OR', label: 'nebo'}
+    ]
 
-  ngOnInit() {
-    this.criteria.push(new Criterium());
-    this.route.params
-      .switchMap((params: Params) => Observable.of(params['criteria'])).subscribe(criteria => {
-        if (criteria) {
-          this.criteria = [];
-          let j = JSON.parse(criteria);
-          for (let i in j) {
-            let c: Criterium = new Criterium();
-          
-            Object.assign(c, j[i]);
-          
-            this.criteria.push(c);
-          }
-          
-          //this.onSearch.emit(this.criteria);
-          if (this.state.config) {
-                this.service.searchFired(this.criteria);
-                } else {
+    constructor(
+        private state: AppState,
+        private service: AppService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {}
 
-            this.subscriptions.push(this.state.configSubject.subscribe(
-              () => {
-                this.service.searchFired(this.criteria);
-              }
-            ));
-          }
+    ngOnInit() {
+        this.criteria.push(new Criterium());
+        this.route.params
+            .switchMap((params: Params) => Observable.of(params['criteria'])).subscribe(criteria => {
+                if (criteria) {
+                    this.criteria = [];
+                    let j = JSON.parse(criteria);
+                    for (let i in j) {
+                        let c: Criterium = new Criterium();
+
+                        Object.assign(c, j[i]);
+
+                        this.criteria.push(c);
+                    }
+
+                    //this.onSearch.emit(this.criteria);
+                    if (this.state.config) {
+                        this.service.searchFired(this.criteria);
+                    } else {
+
+                        this.subscriptions.push(this.state.configSubject.subscribe(
+                            () => {
+                                this.service.searchFired(this.criteria);
+                            }
+                        ));
+                    }
+                }
+            });
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach((s: Subscription) => {
+            s.unsubscribe();
+        });
+        this.subscriptions = [];
+    }
+
+    setField(criterium: Criterium, field: string) {
+        criterium.field = field;
+    }
+
+    getLabel(criterium: Criterium): string {
+        for (let i in this.fields) {
+            if (criterium.field === this.fields[i].field) {
+                return this.fields[i].label;
+            }
         }
-      });
-  }
 
-  ngOnDestroy() {
-    this.subscriptions.forEach((s: Subscription) => {
-      s.unsubscribe();
-    });
-    this.subscriptions = [];
-  }
-  
-  setField(criterium: Criterium, field: string){
-    criterium.field = field;
-  }
-  
-  getLabel(criterium: Criterium): string{
-    for(let i in this.fields){
-      if (criterium.field === this.fields[i].field){
-        return this.fields[i].label;
-      }
+        return 'kdekoliv';
     }
-    
-    return 'kdekoliv';
-  }
-  
-  setOperator(criterium: Criterium, val: string){
-    criterium.operator = val;
-  }
-  
-  getOperator(criterium: Criterium): string{
-    for (let i in this.operators){
-      if (criterium.operator === this.operators[i].val){
-        return this.operators[i].label;
-      }
+
+    setOperator(criterium: Criterium, val: string) {
+        criterium.operator = val;
     }
-    return 'a zároveň';
-    
-  }
 
-  addCriterium() {
-    this.criteria.push(new Criterium());
-  }
+    getOperator(criterium: Criterium): string {
+        for (let i in this.operators) {
+            if (criterium.operator === this.operators[i].val) {
+                return this.operators[i].label;
+            }
+        }
+        return 'a zároveň';
 
-  removeCriterium(i: number) {
-    this.criteria.splice(i, 1);
-    this.search();
-  }
-  
-  reset(){
-    this.criteria = [];
-    this.criteria.push(new Criterium());
-  }
+    }
 
-  search() {
-    let p = {};
-    Object.assign(p, this.route.snapshot.params);
-    p['criteria'] = JSON.stringify(this.criteria);
-    p['start'] = 0;
-    this.router.navigate(['/hledat/cokoliv', p]);
-  }
+    addCriterium() {
+        this.criteria.push(new Criterium());
+    }
+
+    removeCriterium(i: number) {
+        this.criteria.splice(i, 1);
+        this.search();
+    }
+
+    reset() {
+        this.criteria = [];
+        this.criteria.push(new Criterium());
+    }
+
+    search() {
+        let p = {};
+        Object.assign(p, this.route.snapshot.params);
+        p['criteria'] = JSON.stringify(this.criteria);
+        p['start'] = 0;
+        this.router.navigate(['/hledat/cokoliv', p]);
+    }
 
 }
--- searchapp/src/main/ngClient/src/app/components/search/search.component.ts
+++ searchapp/src/main/ngClient/src/app/components/search/search.component.ts
@@ -1,264 +1,302 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {Component, OnInit, OnDestroy, ViewChild, ElementRef} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
-import { Router, ActivatedRoute, Params, NavigationStart, NavigationEnd } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
-import { Subscription } from 'rxjs/Subscription';
-import { NouisliderComponent } from 'ng2-nouislider';
+import {Router, ActivatedRoute, Params, NavigationStart, NavigationEnd} from '@angular/router';
+import {Observable} from 'rxjs/Rx';
+import {Subscription} from 'rxjs/Subscription';
+import {NouisliderComponent} from 'ng2-nouislider';
 
-import { URLSearchParams } from '@angular/http';
+import {URLSearchParams} from '@angular/http';
 
-import { Criterium } from '../../models/criterium';
-import { SearchService } from '../../services/search.service';
+import {Criterium} from '../../models/criterium';
+import {SearchService} from '../../services/search.service';
 
-import { AppService } from '../../services/app.service';
-import { AppState } from '../../app.state';
+import {AppService} from '../../services/app.service';
+import {AppState} from '../../app.state';
 
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
-  @ViewChild('results') results: ElementRef;
-  @ViewChild('dateSlider') public dateSlider: NouisliderComponent;
-  docs: any[];
-  numFound: number;
-  totalPages: number = 0;
+    @ViewChild('results') results: ElementRef;
+    @ViewChild('dateSlider') public dateSlider: NouisliderComponent;
+    docs: any[];
+    numFound: number;
+    totalPages: number = 0;
 
-  start: number = 0;
-  rowsSelect: number[] = [10, 20, 30];
-  rows: number = 10;
-  dateMin: number = 0;
-  dateMax: number = 1;
-  dateOd: number = 0;
-  dateDo: number = 1;
-  dateRange: number[] = [0, 1];
-  
-  currentSort: any;
-  
-  onlyPeerReviewed: boolean = false;
+    start: number = 0;
+    rowsSelect: number[] = [10, 20, 30];
+    rows: number = 10;
+    dateMin: number = 2000;
+    dateMax: number = 2019;
+    dateOd: number = 2000;
+    dateDo: number = 2019;
+    dateRange: number[] = [0, 1];
+    hasDateFilter: boolean = false;
 
-  public dateForm: FormGroup;
+    currentSort: any;
 
-  subscriptions: Subscription[] = [];
+    onlyPeerReviewed: boolean = false;
 
-  constructor(
-    private service: AppService,
-    private state: AppState,
-    private router: Router,
-    private route: ActivatedRoute,
-    private searchService: SearchService,
-    private formBuilder: FormBuilder
-  ) { }
+    //  public dateForm: FormGroup;
 
-  ngOnInit() {
+    subscriptions: Subscription[] = [];
 
-    this.currentSort = this.state.sorts[0];
-    this.subscriptions.push(this.service.searchSubject.subscribe((criteria: Criterium[]) => this.search(criteria)));
+    constructor(
+        private service: AppService,
+        private state: AppState,
+        private router: Router,
+        private route: ActivatedRoute,
+        private searchService: SearchService,
+        private formBuilder: FormBuilder
+    ) {}
 
-    this.getStats();
-    this.subscriptions.push(this.router.events.subscribe(val => {
-      if (val instanceof NavigationEnd) {
-        if (this.route.snapshot.firstChild.params.hasOwnProperty('start')) {
-          this.start = +this.route.snapshot.firstChild.params['start'];
-        }
-        if (this.route.snapshot.firstChild.params.hasOwnProperty('rows')) {
-          this.rows = +this.route.snapshot.firstChild.params['rows'];
-        }
-        if (this.route.snapshot.firstChild.params.hasOwnProperty('sort')) {
-          let s = this.route.snapshot.firstChild.params['sort'];
-          for (let i in this.state.sorts){
-            //console.log(this.state.sorts[i].field);
-            if (this.state.sorts[i].field === s){
-              this.currentSort = this.state.sorts[i];
-              break
+    ngOnInit() {
+
+        this.currentSort = this.state.sorts[0];
+        this.subscriptions.push(this.service.searchSubject.subscribe((criteria: Criterium[]) => this.search(criteria)));
+
+        this.getStats();
+        this.subscriptions.push(this.router.events.subscribe(val => {
+            if (val instanceof NavigationEnd) {
+                if (this.route.snapshot.firstChild.params.hasOwnProperty('start')) {
+                    this.start = +this.route.snapshot.firstChild.params['start'];
+                }
+                if (this.route.snapshot.firstChild.params.hasOwnProperty('rows')) {
+                    this.rows = +this.route.snapshot.firstChild.params['rows'];
+                }
+                if (this.route.snapshot.firstChild.params.hasOwnProperty('sort')) {
+                    let s = this.route.snapshot.firstChild.params['sort'];
+                    for (let i in this.state.sorts) {
+                        //console.log(this.state.sorts[i].field);
+                        if (this.state.sorts[i].field === s) {
+                            this.currentSort = this.state.sorts[i];
+                            break
+                        }
+                    }
+                    //console.log(s, this.currentSort);
+                }
+                if (this.route.snapshot.firstChild.params.hasOwnProperty('onlyPeerReviewed')) {
+                    this.onlyPeerReviewed = this.route.snapshot.firstChild.params['onlyPeerReviewed'] === 'true';
+                }
+
+                if (this.route.snapshot.firstChild.params.hasOwnProperty('date')) {
+                    let date = this.route.snapshot.firstChild.params['date'];
+                    if (date) {
+
+                        this.hasDateFilter =                         true;
+//      this.dateForm = this.formBuilder.group({ 'range': [[this.dateMin, this.dateMax]] });
+
+                        let j = JSON.parse(date);
+                        this.changeRangeFormValue(j[0], j[1]);
+                        if (this.state.config) {
+                            this.search([]);
+                        } else {
+                            let sss = this.state.configSubject.subscribe(() => {
+                                this.search([]);
+                                sss.unsubscribe();
+                            });
+                        }
+
+                    }
+                }
+            } else if (val instanceof NavigationStart) {
+
             }
-          }
-          //console.log(s, this.currentSort);
-        }
-        if (this.route.snapshot.firstChild.params.hasOwnProperty('onlyPeerReviewed')) {
-          this.onlyPeerReviewed = this.route.snapshot.firstChild.params['onlyPeerReviewed'] === 'true';
-        }
-      } else if (val instanceof NavigationStart) {
+        }));
+        this.subscriptions.push(this.route.params
+            .switchMap((params: Params) => Observable.of(params['start'])).subscribe(start => {
+                if (start) {
+                    this.start = +start;
+                }
+            }));
 
-      }
-    }));
-    this.subscriptions.push(this.route.params
-      .switchMap((params: Params) => Observable.of(params['start'])).subscribe(start => {
-        if (start) {
-          this.start = +start;
-        }
-      }));
-
-    this.subscriptions.push(this.route.params
-      .switchMap((params: Params) => Observable.of(params['date'])).subscribe(date => {
-        if (date) {
-          let j = JSON.parse(date);
-          this.changeRangeFormValue(j[0], j[1]);
-        }
-      }));
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach((s: Subscription) => {
-      s.unsubscribe();
-    });
-    this.subscriptions = [];
-  }
-
-  showResults() {
-    let s = this.route.snapshot.children[0].url[0].path;
-    return s.indexOf('cokoliv') > -1;
-  }
-
-  lastResult() {
-    return Math.min(this.start + this.rows, this.numFound);
-  }
-
-  search(criteria: Criterium[]) {
-    this.numFound = 0;
-
-    var params = new URLSearchParams();
-    params.set('q', '*:*');
-    params.set('q.op', 'AND');
-    params.set('fq', 'model:article');
-    params.set('start', this.start + '');
-    params.set('rows', this.rows + '');
-    params.set('sort', this.currentSort.field);
-    if (criteria.length > 0) {
-      let fq = '';
-      let op = 'AND';
-      for (let i = 0; i < criteria.length; i++) {
-        if(criteria[i].value){
-          if (fq !== '') {
-            fq += criteria[i-1].operator + ' ';
-          }
-          if (criteria[i].field) {
-            fq += criteria[i].field + ':' + criteria[i].value + ' ';
-          } else {
-            fq += criteria[i].value + ' ';
-          }
-        }
-      }
-      params.append('fq', fq.trim());
+        this.subscriptions.push(this.route.params
+            .switchMap((params: Params) => Observable.of(params['date'])).subscribe(date => {
+                if (date) {
+                    let j = JSON.parse(date);
+                    this.changeRangeFormValue(j[0], j[1]);
+                }
+            }));
     }
 
-    //Add date filter
-    if(this.dateForm){
-      params.append('fq', 'year:[' + this.dateForm.controls['range'].value[0] + ' TO ' + this.dateForm.controls['range'].value[1] + ']');
-    }
-    
-    //Add onlyPeerReviewed
-    if(this.onlyPeerReviewed){
-      params.append('fq', 'genre:"peer-reviewed"')
+    ngOnDestroy() {
+        this.subscriptions.forEach((s: Subscription) => {
+            s.unsubscribe();
+        });
+        this.subscriptions = [];
     }
 
-    //console.log(params.toString());
+    showResults() {
+        let s = this.route.snapshot.children[0].url[0].path;
+        return s.indexOf('cokoliv') > -1;
+    }
 
-    //Rok jako stats
-    params.set('stats', 'true');
-    params.set('stats.field', 'year');
+    lastResult() {
+        return Math.min(this.start + this.rows, this.numFound);
+    }
 
-    this.searchService.search(params).subscribe(res => {
+    search(criteria: Criterium[]) {
+        this.numFound = 0;
 
-      this.docs = res['response']['docs'];
-      this.numFound = res['response']['numFound'];
-      this.totalPages = Math.ceil(this.numFound / this.rows);
+        var params = new URLSearchParams();
+        params.set('q', '*:*');
+        params.set('q.op', 'AND');
+        params.set('fq', 'model:article');
+        params.set('start', this.start + '');
+        params.set('rows', this.rows + '');
+        params.set('sort', this.currentSort.field);
+        if (criteria.length > 0) {
+            let fq = '';
+            let op = 'AND';
+            for (let i = 0; i < criteria.length; i++) {
+                if (criteria[i].value) {
+                    if (fq !== '') {
+                        fq += criteria[i - 1].operator + ' ';
+                    }
+                    if (criteria[i].field) {
+                        fq += criteria[i].field + ':' + criteria[i].value + ' ';
+                    } else {
+                        fq += criteria[i].value + ' ';
+                    }
+                }
+            }
+            params.append('fq', fq.trim());
 
-      if (this.numFound == 0) {
-        this.changeRangeFormValue(this.dateMin, this.dateMax);
-      } else if (res.hasOwnProperty('stats') && res['stats']['stats_fields'].hasOwnProperty('year')) {
-        this.changeRangeFormValue(res['stats']['stats_fields']['year']['min'], res['stats']['stats_fields']['year']['max']);
-      }
-
-      //this.results.nativeElement.scrollIntoView();
-
-    });
-  }
-
-  getStats() {
-    if (this.state.config) {
-      this.dateMin = 2000;
-      this.dateMax = 2017;
-      this.dateForm = this.formBuilder.group({ 'range': [[this.dateMin, this.dateMax]] });
-
-      var params = new URLSearchParams();
-      params.set('q', '*:*');
-      params.set('rows', '0');
-      //Rok jako stats
-      params.set('stats', 'true');
-      params.set('stats.field', 'year');
-
-      this.searchService.search(params).subscribe(res => {
-        if (res.hasOwnProperty('stats') && res['stats']['stats_fields'].hasOwnProperty('year')) {
-          this.dateMin = res['stats']['stats_fields']['year']['min'];
-          this.dateMax = res['stats']['stats_fields']['year']['max'];
-          this.dateForm = this.formBuilder.group({ 'range': [[this.dateMin, this.dateMax]] });
-
+            //Rok jako stats
+            params.set('stats', 'true');
+            params.set('stats.field', 'year');
         }
 
-      });
-    } else {
-      this.subscriptions.push(this.state.stateChangedSubject.subscribe(
-        () => {
-          this.getStats();
+        //Add date filter
+        if (this.dateOd) {
+            params.append('fq', 'year:[' + this.dateOd + ' TO ' + this.dateDo + ']');
         }
-      ));
+
+        //Add onlyPeerReviewed
+        if (this.onlyPeerReviewed) {
+            params.append('fq', 'genre:"peer-reviewed"')
+        }
+
+        //console.log(params.toString());
+
+
+        this.searchService.search(params).subscribe(res => {
+            this.docs = res['response']['docs'];
+            this.numFound = res['response']['numFound'];
+            this.totalPages = Math.ceil(this.numFound / this.rows);
+
+            if (this.numFound == 0) {
+                this.changeRangeFormValue(this.dateMin, this.dateMax);
+            } else if (res.hasOwnProperty('stats') && res['stats']['stats_fields'].hasOwnProperty('year')) {
+                //this.changeRangeFormValue(res['stats']['stats_fields']['year']['min'], res['stats']['stats_fields']['year']['max']);
+            }
+
+            //this.results.nativeElement.scrollIntoView();
+
+        });
     }
-  }
 
-  changeRangeFormValue(dateOd: number, dateDo: number) {
-    const control = <FormControl>this.dateForm.controls['range'];
-    const newRange = control.value;
-    newRange[0] = dateOd;
-    newRange[1] = dateDo;
-    control.setValue(newRange);
-  }
+    getStats() {
+        if (this.state.config) {
+            this.dateMin = 2000;
+            this.dateMax = 2019;
+                              //this.dateForm = this.formBuilder.group({ 'range': [[this.dateMin, this.dateMax]] });
 
-  onDateChange(e) {
-    let p = {};
-    Object.assign(p, this.route.snapshot.firstChild.params);
-    p['date'] = JSON.stringify(this.dateForm.controls['range'].value);
-    this.router.navigate(['/hledat/cokoliv', p]);
-    return;
-  }
+            var params = new URLSearchParams();
+            params.set('q', '*:*');
+            params.set('rows', '0');
+            //Rok jako stats
+            params.set('stats', 'true');
+            params.set('stats.field', 'year');
+
+            this.searchService.search(params).subscribe(res => {
+                if (res.hasOwnProperty('stats') && res['stats']['stats_fields'].hasOwnProperty('year')) {
+                    this.dateMin = res['stats']['stats_fields']['year']['min'];
+                    this.dateMax = res['stats']['stats_fields']['year']['max'];
+                    if (!this.hasDateFilter) {
+                        this.dateOd = this.dateMin;
+                        this.dateDo = this.dateMax;
+                        this.dateRange = [this.dateOd, this.dateDo];
+                    }
+                    //this.dateForm = this.formBuilder.group({ 'range': [[this.dateMin, this.dateMax]] });
+
+                }
+
+            });
+        } else {
+            this.subscriptions.push(this.state.configSubject.subscribe(
+                () => {
+                    this.getStats();
+                }
+            ));
+        }
+    }
+
+    changeRangeFormValue(dateOd: number, dateDo: number) {
+        //    const control = <FormControl>this.dateForm.controls['range'];
+        //    const newRange = control.value;
+        //    newRange[0] = dateOd;
+        //    newRange[1] = dateDo;
+        //    control.setValue(newRange);
+        this.dateOd = dateOd;
+        this.dateDo = dateDo;
+        this.dateRange = [dateOd, dateDo];
+    }
+
+    dateChange() {
+        this.onDateChange([this.dateOd, this.dateDo]);
+    }
+    onDateChange(e) {
+        if (e) {
+            this.changeRangeFormValue(e[0], e[1]);
+        }
+        let p = {};
+        Object.assign(p, this.route.snapshot.firstChild.params);
+        //p['date'] = JSON.stringify(this.dateForm.controls['range'].value);
+        p['date'] = JSON.stringify(this.dateRange);
+        p['start'] = 0;
+        this.router.navigate(['/hledat/cokoliv', p]);
+        return;
+    }
 
 
-  setPage(page: number) {
-    this.start = page * this.rows;
-    let p = {};
-    Object.assign(p, this.route.snapshot.firstChild.params);
-    console.log(p)
-    p['start'] = this.start;
-    this.router.navigate(['/hledat/cokoliv', p]);
-  }
+    setPage(page: number) {
+        this.start = page * this.rows;
+        let p = {};
+        Object.assign(p, this.route.snapshot.firstChild.params);
+        console.log(p)
+        p['start'] = this.start;
+        this.router.navigate(['/hledat/cokoliv', p]);
+    }
 
-  setRows(r: number) {
-    this.rows = r;
-    let p = {};
-    Object.assign(p, this.route.snapshot.firstChild.params);
-    p['rows'] = this.rows;
-    this.router.navigate(['/hledat/cokoliv', p]);
-  }
-  
-  setSort(s: any) {
-    this.currentSort = s;
-    let p = {};
-    Object.assign(p, this.route.snapshot.firstChild.params);
-    p['sort'] = this.currentSort.field;
-    this.router.navigate(['/hledat/cokoliv', p]);
-  }
-  
-  setPeerReviewed(){
-    //this.rows = r;
-    let p = {};
-    Object.assign(p, this.route.snapshot.firstChild.params);
-    p['onlyPeerReviewed'] = this.onlyPeerReviewed;
-    this.router.navigate(['/hledat/cokoliv', p]);
-  }
+    setRows(r: number) {
+        this.rows = r;
+        let p = {};
+        Object.assign(p, this.route.snapshot.firstChild.params);
+        p['rows'] = this.rows;
+        this.router.navigate(['/hledat/cokoliv', p]);
+    }
+
+    setSort(s: any) {
+        this.currentSort = s;
+        let p = {};
+        Object.assign(p, this.route.snapshot.firstChild.params);
+        p['sort'] = this.currentSort.field;
+        this.router.navigate(['/hledat/cokoliv', p]);
+    }
+
+    setPeerReviewed() {
+        //this.rows = r;
+        let p = {};
+        Object.assign(p, this.route.snapshot.firstChild.params);
+        p['onlyPeerReviewed'] = this.onlyPeerReviewed;
+        this.router.navigate(['/hledat/cokoliv', p]);
+    }
 
 
 }

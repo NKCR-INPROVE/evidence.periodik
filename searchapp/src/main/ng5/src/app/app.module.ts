4277197a5a8201c3aca2e61f844e4bf4178863a9
Alberto Hernandez <alberto@albertoX1>
2018-04-25 15:02:05 +0200

    scss compiling

--- searchapp/src/main/ng5/src/app/app.module.ts
+++ searchapp/src/main/ng5/src/app/app.module.ts
@@ -1,54 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-import { BsDropdownModule, ModalModule, CollapseModule, TypeaheadModule, TooltipModule, AlertModule } from 'ngx-bootstrap';
-import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {BsDropdownModule, ModalModule, CollapseModule, TypeaheadModule, TooltipModule, AlertModule} from 'ngx-bootstrap';
+import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { NouisliderModule } from 'ng2-nouislider';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
+import {NouisliderModule} from 'ng2-nouislider';
 
-import { FileUploadModule } from 'ng2-file-upload';
+import {FileUploadModule} from 'ng2-file-upload';
 
-import { AppState } from './app.state';
-import { AppService } from './services/app.service';
-import { SearchService } from './services/search.service';
+import {AppState} from './app.state';
+import {AppService} from './services/app.service';
+import {SearchService} from './services/search.service';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { OCasopisuComponent } from './components/o-casopisu/o-casopisu.component';
-import { PokynyComponent } from './components/pokyny/pokyny.component';
-import { ArchivComponent } from './components/archiv/archiv.component';
-import { SearchComponent } from './components/search/search.component';
-import { HomeComponent } from './components/home/home.component';
-import { FreeTextComponent } from './components/free-text/free-text.component';
-import { JournalDetailsComponent } from './components/journal-details/journal-details.component';
-import { ArticleResultComponent } from './components/article-result/article-result.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { ArticleViewerComponent } from './components/article-viewer/article-viewer.component';
-import { ArchivItemComponent } from './components/archiv-item/archiv-item.component';
-import { ArchivItemLeftComponent } from './components/archiv-item-left/archiv-item-left.component';
-import { SearchCriteriaComponent } from './components/search-criteria/search-criteria.component';
-import { SearchTabsComponent } from './components/search-tabs/search-tabs.component';
-import { SearchAuthorsComponent } from './components/search-authors/search-authors.component';
-import { SearchGenresComponent } from './components/search-genres/search-genres.component';
-import { SearchKeywordsComponent } from './components/search-keywords/search-keywords.component';
-import { PaginationComponent } from './components/pagination/pagination.component';
-import { ArticleInfoComponent } from './components/article-info/article-info.component';
-import { ActualComponent } from './components/actual/actual.component';
-import { FreePageComponent } from './components/free-page/free-page.component';
-import { LoginComponent } from './components/login/login.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { AuthGuard } from "./services/auth-guard";
-import { SafeHtmlPipe } from './services/safe-html.pipe';
-import { TestComponent } from './components/test/test.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {OCasopisuComponent} from './components/o-casopisu/o-casopisu.component';
+import {PokynyComponent} from './components/pokyny/pokyny.component';
+import {ArchivComponent} from './components/archiv/archiv.component';
+import {SearchComponent} from './components/search/search.component';
+import {HomeComponent} from './components/home/home.component';
+import {FreeTextComponent} from './components/free-text/free-text.component';
+import {JournalDetailsComponent} from './components/journal-details/journal-details.component';
+import {ArticleResultComponent} from './components/article-result/article-result.component';
+import {BreadcrumbsComponent} from './components/breadcrumbs/breadcrumbs.component';
+import {ArticleViewerComponent} from './components/article-viewer/article-viewer.component';
+import {ArchivItemComponent} from './components/archiv-item/archiv-item.component';
+import {ArchivItemLeftComponent} from './components/archiv-item-left/archiv-item-left.component';
+import {SearchCriteriaComponent} from './components/search-criteria/search-criteria.component';
+import {SearchTabsComponent} from './components/search-tabs/search-tabs.component';
+import {SearchAuthorsComponent} from './components/search-authors/search-authors.component';
+import {SearchGenresComponent} from './components/search-genres/search-genres.component';
+import {SearchKeywordsComponent} from './components/search-keywords/search-keywords.component';
+import {PaginationComponent} from './components/pagination/pagination.component';
+import {ArticleInfoComponent} from './components/article-info/article-info.component';
+import {ActualComponent} from './components/actual/actual.component';
+import {FreePageComponent} from './components/free-page/free-page.component';
+import {LoginComponent} from './components/login/login.component';
+import {AdminComponent} from './components/admin/admin.component';
+import {AuthGuard} from "./services/auth-guard";
+import {SafeHtmlPipe} from './services/safe-html.pipe';
+import {TestComponent} from './components/test/test.component';
 
 export function createTranslateLoader(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
@@ -92,54 +92,62 @@ export function createTranslateLoader(http: HttpClient) {
     TypeaheadModule.forRoot(),
     TooltipModule.forRoot(),
     AlertModule.forRoot(),
-  FileUploadModule,
-    
-  TranslateModule.forRoot({
-          loader: {
-                provide: TranslateLoader,
-                useFactory: (createTranslateLoader),
-              deps: [HttpClient]
-          }
-      }),
-    //StoreModule, storeManager,
+    FileUploadModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
+
     RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      { path: 'test', component: TestComponent },
-      { path: 'actual', component: ActualComponent },
-      { path: 'archiv', component: ArchivComponent },
+      {path: 'home', component: HomeComponent},
+      {path: 'test', component: TestComponent},
+      {path: 'actual', component: ActualComponent},
+      {path: 'archiv', component: ArchivComponent},
       //{ path: 'archiv/:pid', component: ArchivComponent },
-      { path: 'pro-autory', component: PokynyComponent ,
-      children:[
-        { path: '', component: FreePageComponent},
-        { path: '**', component: FreePageComponent}
-        
-      ]},
-      { path: 'o-casopisu', component: OCasopisuComponent,
-      children:[
-        { path: '', component: FreePageComponent},
-        { path: '**', component: FreePageComponent}
-        
-      ]},
-      { path: 'kontakt', component: FreePageComponent },
-      { path: 'e-shop', component: FreePageComponent },
-      { path: 'hledat', component: SearchComponent,
-      children:[
-        { path: '', redirectTo: 'cokoliv', pathMatch: 'full'},
-        { path: 'cokoliv', component: SearchCriteriaComponent},
-        { path: 'cokoliv/:criteria', component: SearchCriteriaComponent },
-        { path: 'autory', component: SearchAuthorsComponent},
-        { path: 'keywords', component: SearchKeywordsComponent},
-        { path: 'rubriky', component: SearchGenresComponent}
-        
-      ]},
-      { path: 'article/:pid', component: ArticleViewerComponent },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'prihlaseni', component: LoginComponent },
-      { path: 'admin', 
-        canActivate: [AuthGuard],component: AdminComponent },
+      {
+        path: 'pro-autory', component: PokynyComponent,
+        children: [
+          {path: '', component: FreePageComponent},
+          {path: '**', component: FreePageComponent}
+
+        ]
+      },
+      {
+        path: 'o-casopisu', component: OCasopisuComponent,
+        children: [
+          {path: '', component: FreePageComponent},
+          {path: '**', component: FreePageComponent}
+
+        ]
+      },
+      {path: 'kontakt', component: FreePageComponent},
+      {path: 'e-shop', component: FreePageComponent},
+      {
+        path: 'hledat', component: SearchComponent,
+        children: [
+          {path: '', redirectTo: 'cokoliv', pathMatch: 'full'},
+          {path: 'cokoliv', component: SearchCriteriaComponent},
+          {path: 'cokoliv/:criteria', component: SearchCriteriaComponent},
+          {path: 'autory', component: SearchAuthorsComponent},
+          {path: 'keywords', component: SearchKeywordsComponent},
+          {path: 'rubriky', component: SearchGenresComponent}
+
+        ]
+      },
+      {path: 'article/:pid', component: ArticleViewerComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'prihlaseni', component: LoginComponent},
+      {
+        path: 'admin',
+        canActivate: [AuthGuard], component: AdminComponent
+      },
     ])
   ],
   providers: [AppState, AppService, SearchService, AuthGuard, TranslateService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
